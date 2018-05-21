ee2922799f9d27fc952b0dc96f086b19ec631fc5
sak39 <sak39@users.noreply.github.com>
2018-05-21 17:59:27 +0900

    Update init.sh

--- init.sh
+++ init.sh
@@ -6,14 +6,17 @@ function set_config() {
 
     SWATCH_CONF_URL="https://raw.githubusercontent.com/yousan/swatch/master/etc/secure.conf"
     curl $SWATCH_CONF_URL > $SWATCH_CONF_DIR/secure.conf
-    chmod 644 $SWATCH_CONF_DIR/secure.conf
+    chmod 0644 $SWATCH_CONF_DIR/secure.conf
+    
     echo "saved into $SWATCH_CONF_DIR/secure.conf"
 }
 
 function set_script() {
-    AUTH_ALERT_SCRIPT_URL="https://raw.githubusercontent.com/yousan/swatch/master/etc/slack_notify.sh"
-    curl $AUTH_ALERT_SCRIPT_URL > /usr/bin/slack_notify.sh
-    chmod 755 /usr/bin/slack_notify.sh
+    read -p "YOUR SLACK INCOMING WEBHOOK URI:" uri
+    SLACK_NOTIFY_SCRIPT_URL="https://raw.githubusercontent.com/yousan/swatch/master/etc/slack_notify.sh"
+    curl $SLACK_NOTIFY_SCRIPT_URL | sed -e "s/<YOUR_INCOMING_WEBHOOK_URI>/$uri/g" | sudo tee /usr/bin/slack_notify.sh
+    chmod 0755 /usr/bin/slack_notify.sh
+    
     echo "saved into /usr/bin/slack_notify.sh"
 }
 
@@ -29,6 +32,8 @@ function set_crontab() {
         echo "Sorry, I can handle Ubuntu and CentOS only."
         exit 1
     fi
+    chmod 0644 /etc/cron.d/swatchron
+    
     echo "saved into /etc/cron.d/swatchron"
 }
 
@@ -36,6 +41,3 @@ function set_crontab() {
 set_config
 set_script
 set_crontab
-
-# you can run using below command,
-# ./etc/swatch.sh
