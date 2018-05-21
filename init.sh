5f3a815ed115a16c88e4061f8125f896672ca48f
sak39 <dnsn.isk@gmail.com>
2018-05-21 16:36:19 +0900

    Fix redirect bugs

--- init.sh
+++ init.sh
@@ -3,24 +3,27 @@ SWATCH_CONF_DIR=/etc/swatch/conf
 [ ! -d $SWATCH_CONF_DIR ] && mkdir -p $SWATCH_CONF_DIR && cd $SWATCH_CONF_DIR
 
 SWATCH_CONF_URL="https://raw.githubusercontent.com/yousan/swatch/master/etc/secure.conf"
-curl $SWATCH_CONF_URL > secure.conf
+curl $SWATCH_CONF_URL | tee $SWATCH_CONF_DIR/secure.conf
 chmod 644 $SWATCH_CONF_DIR/secure.conf
+echo "saved into $SWATCH_CONF_DIR/secure.conf"
 
 AUTH_ALERT_SCRIPT_URL="https://raw.githubusercontent.com/yousan/swatch/master/etc/authAlert.sh"
-curl $ALERT_SCRIPT_URL > /usr/bin/authAlert.sh
+curl $ALERT_SCRIPT_URL | tee /usr/bin/authAlert.sh
 chmod 755 /usr/bin/authAlert.sh
+echo "saved into /usr/bin/authAlert.sh"
 
 SWATCH_CRON_UBUNTU_URL="https://raw.githubusercontent.com/yousan/swatch/master/etc/swatchron.ubuntu"
 SWATCH_CRON_CENTOS_URL="https://raw.githubusercontent.com/yousan/swatch/master/etc/swatchron.centos"
 DISTRIBUTION=$(lsb_release -i)
 if [[ $DISTRIBUTION =~ Ubuntu  ]]; then
-    curl $SWATCH_CRON_UBUNTU_URL > /etc/cron.d/swatchron.ubuntu
+    curl $SWATCH_CRON_UBUNTU_URL | tee /etc/cron.d/swatchron
 elif [[ $DISTRIBUTION =~ CentOS ]]; then
-    curl $SWATCH_CRON_CENTOS_URL > /etc/cron.d/swatchron.centos
+    curl $SWATCH_CRON_CENTOS_URL | tee /etc/cron.d/swatchron
 else
     echo "Sorry, I can handle Ubuntu and CentOS only."
     exit 1
 fi
+echo "saved into /etc/cron.d/swatchron"
 
 # you can run using below command,
 # ./etc/swatch.sh
